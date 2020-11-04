6dc06e3ab306550d363274cdd454b82db66ce949
victoire <victoire@192.168.0.14>
2020-11-04 16:01:32 +0100

    Continuation partie

--- src/projet_p4/Partie.java
+++ src/projet_p4/Partie.java
@@ -14,32 +14,24 @@ import java.util.Scanner;
 public class Partie {
     
     Joueur[] ListeJoueurs = new Joueur[2];
-    Jeton[] ListeJetons = new Jeton[21];
     Joueur joueurActuel;
-    Grille[][] grille = new Grille[6][7];
+    Grille grilleJeu = new Grille();
     
     public void attribuerCouleursAuxJoueurs(){
-        
-        String Couleur[] = new String[2];
-        Couleur[0] = "Jaune";
-        Couleur[1]= "Rouge";
-        
         Random nbAleat = new Random();
-        int nb = nbAleat.nextInt(1);
-       
-        ListeJoueurs[0].CouleurJoueur = Couleur[nb];
-        
-        if ("Jaune".equals(ListeJoueurs[0].CouleurJoueur)){
-            ListeJoueurs[1].CouleurJoueur = "Rouge";
-        }
-        else{
+        boolean couleurJeton;
+        couleurJeton = nbAleat.nextBoolean();
+        if (couleurJeton){
+            ListeJoueurs[0].CouleurJoueur = "Rouge";
             ListeJoueurs[1].CouleurJoueur = "Jaune";
         }
-        
-        System.out.println("La couleur du joueur 1 est : " + ListeJoueurs[0].CouleurJoueur+" et la couleur du joueur 2 est : " + ListeJoueurs[1].CouleurJoueur);
-    }
+        else{
+            ListeJoueurs[0].CouleurJoueur = "Jaune";
+            ListeJoueurs[1].CouleurJoueur = "Rouge";
+        }
+    }   
     
-    public Joueur ProchainJoueur(Joueur joueur_1){
+    public Joueur ProchainJoueur(Joueur unJoueur){
         if(ListeJoueurs[0]== joueurActuel){
             return ListeJoueurs[1];
         }
@@ -50,7 +42,44 @@ public class Partie {
     
     public void initialiserPartie(){
         //on met ici en place notre grille
+        grilleJeu.viderGrille();
         
+        //on cree ici les joueurs 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choix du pseudo du premier joueur : ");
+        Joueur Joueur1 = new Joueur(sc.nextLine());
+        System.out.println("Choix du pseudo du deuxième joueur : ");
+        Joueur Joueur2 = new Joueur(sc.nextLine());
+        ListeJoueurs[0] = Joueur1;
+        ListeJoueurs[1] = Joueur2;
+        
+        attribuerCouleursAuxJoueurs();
+        System.out.println(Joueur1.Nom + " a pour couleur "+Joueur1.CouleurJoueur);
+        System.out.println(Joueur2.Nom + " a pour couleur "+Joueur2.CouleurJoueur);
+        
+        //on assimile ensuite des jetons aux deux joueurs
+        int Compteur =0;
+        for (int i=0; 1<5; i++){
+            int ligneTrouNoir = nbAleat.nextInt(6);
+            int colonneTrouNoir = nbAleat.nextInt(7);
+            if(Compteur<2){
+                if (!grilleJeu.placerTrouNoir(ligneTrouNoir, colonneTrouNoir)){
+                    Compteur = Compteur--;
+                }
+                else{
+                    Compteur = Compteur + 1;
+                }
+            }   
+            if(!grilleJeu.placerTrouNoir(ligneTrouNoir, colonneTrouNoir)){
+                i--;
+            }
+            
+        }
+        
+        //on place alors les 3 derniers desintegrateurs
+        int menu_jouer(){
+            
+        }
         
     }
     

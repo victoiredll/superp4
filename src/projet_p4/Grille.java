5dc7bd6cb34d6d3708504fe83af4dc7f71841929
margo <margo@LAPTOP-GPF3173A.home>
2020-11-04 15:07:13 +0100

    modification grille sans les commentaires

--- src/projet_p4/Grille.java
+++ src/projet_p4/Grille.java
@@ -12,16 +12,37 @@ package projet_p4;
 public class Grille {
     Cellule[][] Cellules = new Cellule[6][7];
     
-    
-    public boolean ajouterJetonDansColonne(Jeton jetonactuel, int nbcolonne){
+    public boolean Colonnevide(Jeton jetonActuel, int nbcolonne){
         boolean colonnevide = true;
-        for (int i=0 ; i<Cellules.length; i++){ //on décrémente d'abord 
-            if (Cellules[i][nbcolonne+1].affecterJeton(jetonactuel)){
+        for (int i=0 ; i<Cellules.length; i++){ 
+            if (Cellules[i][nbcolonne+1].affecterJeton(jetonActuel)){
                 colonnevide = true;
             }
             else {
                 colonnevide = false;
             }
         }
-    return colonnevide; }
+    return colonnevide; 
+    }
+    public boolean ajouterJetonDansColonne(Joueur joueurActuel, int nbcolonne){
+        int i=0;
+        while (Cellules[i][nbcolonne].JetonCourant!=null){
+        i=i+1;
+    }
+        Jeton jetonDuJoueur = joueurActuel.retirer_jeton(); 
+        Cellules[i][nbcolonne].JetonActuel = jetonDuJoueur; 
+        
+        if (Cellules[i][nbcolonne].presenceTrouNoir()){
+            Cellules[i][nbcolonne].activerTrouNoir();
+            
+        }
+        if (Cellules[i][nbcolonne]).presenceDesintegrateur()){
+            Cellules[i][nbcolonne].recupererDesintegrateur();
+            joueurActuel.obtenirDesintegrateur();     
+    }
+        
+     return true;   
+    }
+        
+    
 }

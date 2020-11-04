fa67873c02bb742c2ff4fc2086aaa88f12b7ced8
victoire <victoire@192.168.0.14>
2020-11-04 14:17:47 +0100

    commentaires sur classe jeton

--- src/projet_p4/Jeton.java
+++ src/projet_p4/Jeton.java
@@ -15,16 +15,19 @@ import java.util.Random;
 public class Jeton {
     String CouleurJeton; 
     
+    //on crée le constructeur de la classe
     public Jeton (String couleur_jeton){
         CouleurJeton=couleur_jeton;
     }
  
     public String lireCouleur(){
         
-        String Couleur[] = new String[2];
-        Couleur[0] = "Jaune";
+        //on crée un tableau de deux lignes contenant les deux couleurs de jeton possibles
+        String Couleur[] = new String[2]; 
+        Couleur[0] = "Jaune"; 
         Couleur[1]= "Rouge";
         
+        //la couleur du jeton est aléatoire donc on crée un nb aleatoire correspondant au numero de la case du tableau, et donc a une des deux couleurs
         Random nbAleat = new Random();
         int nb = nbAleat.nextInt(1);
         

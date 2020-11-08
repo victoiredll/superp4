deb826bfce3a395e5b9483c4c6e106ae2bd53ba8
victoire <victoire@192.168.0.14>
2020-11-08 15:17:11 +0100

    commentaire

--- src/projet_p4/Joueur.java
+++ src/projet_p4/Joueur.java
@@ -9,28 +9,28 @@ package projet_p4;
  *
  * @author mlenorma
  */
-public class Joueur { //on cr�e la classe Joueur avec ses attributs 
+public class Joueur { //on cree la classe Joueur avec ses attributs 
     String Nom; 
     String CouleurJoueur; 
-    Jeton[] ListeJetons = new Jeton[21]; //liste compl�te des jetons 
+    Jeton[] ListeJetons = new Jeton[21]; //liste complete des jetons 
     int nombreDesintegrateurs; 
     int nombreJetons_restants; 
     
-    public  Joueur(String unNom) { //creation du constructeur joueur 
+    public  Joueur(String unNom) { //creation du constructeur joueur avec son parametre 
 
         Nom = unNom;
         nombreDesintegrateurs = 0; //on attribue au joueur un nombre de desintegrateur et de jetons 
-        //qui �voluera au cours de la partie
-        //on les initialise � 0
+        //qui evoluera au cours de la partie
+        //on les initialise a 0
         nombreJetons_restants = 0; 
     }
     
-    public boolean ajouter_jeton(Jeton jeton_a_ajouter){ //on ajoute le jeton � la liste
+    public boolean ajouter_jeton(Jeton jeton_a_ajouter){ //on ajoute le jeton a la liste de jetons
         
             ListeJetons[nombreJetons_restants]=jeton_a_ajouter;
             nombreJetons_restants=nombreJetons_restants+1; //on augmente le nombre de jetons restants au joueur 
             
-            if (nombreJetons_restants <=21){ //pour un soucis de coh�rence, il ne peut y avoir que 21 jetons maximum
+            if (nombreJetons_restants <=21){ //pour un soucis de coherence, il ne peut y avoir que 21 jetons maximum
                 return true; 
                 
             }
@@ -40,9 +40,9 @@ public class Joueur { //on cr�e la classe Joueur avec ses attributs
     }   
     public Jeton retirer_jeton (){ //on retire un jeton � la liste 
         nombreJetons_restants=nombreJetons_restants-1; //on diminue le nombre de jetons restants 
-        return ListeJetons[nombreJetons_restants]; //ici, pas besoin de crer un objet car on enl�ve seulement un jeton 
+        return ListeJetons[nombreJetons_restants]; //ici, pas besoin de crer un objet car on enleve seulement un jeton 
         }
-    public void obtenirDesintegrateur(){ //cette fonction permet d'augmenter le nombre de d�sint�grateurs poss�d� par le joueur 
+    public void obtenirDesintegrateur(){ //cette fonction permet d'augmenter le nombre de desintegrateurs possede par le joueur 
         nombreDesintegrateurs= nombreDesintegrateurs+1;
     }
     
